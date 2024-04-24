9aff961507cd1c4c16dae84c8d304b5fd0d2e977
Shashi Mallah <62691361+shashimallah@users.noreply.github.com>
2024-04-25 02:07:01 +0530

    test2

--- src/app/constants/API_CONSTANTS.ts
+++ src/app/constants/API_CONSTANTS.ts
@@ -1,6 +1,6 @@
 export class API_CONSTANTS {
 
-    public static readonly API = '/api';
+    public static readonly API = 'https://dark-mel-cloudstore.koyeb.app/api';
     public static readonly GET_VIDEO_LIST = this.API + '/youtube/find-all';
     public static readonly ADD_NEW_VIDEO = this.API + '/youtube/add-video';
 
